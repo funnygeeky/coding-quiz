bc71bd4f72c80cfb167e544290bdd5b595058586
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-12 18:41:31 -0500

    added conditional statement to toggle color of box 2

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-script.js
 //When mouse over box 2, the color changes to orange
-document.getElementById("box2").addEventListener("mouseover,changeColor");
-function changeColor() {
-    document.getElementById("box2").style.backgroundColor="orange";
-}
+
+//document.getElementById("box2").addEventListener("mouseover",changeColor);
+
+//function changeColor() {
+    //document.getElementById("box2").style.backgroundColor="orange";
+//}
+var box2 = document.getElementById("box2");
+
+box2.addEventListener("mouseover",function()
+{
+    if (box2.onmouseover == true) {
+        box2.style.backgroundColor="orange";
+    }
+    if (box2.onmouseover==false) {
+    box2.style.backgroundColor="blue";
+    }
+});
