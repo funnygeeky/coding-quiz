7bd0b7e4993ad1b0cf11a88a3019b5469de7cbd4
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-12 15:13:32 -0500

    added js listener to box 2 to change color when mouse over

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 script.js
+//When mouse over box 2, the color changes to orange
+document.getElementById("box2").addEventListener("mouseover,changeColor");
+function changeColor() {
+    document.getElementById("box2").style.backgroundColor="orange";
+}
