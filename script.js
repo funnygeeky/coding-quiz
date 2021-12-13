057421a474de10ab7aade9f6039eed2a10b35ffe
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-12 19:26:39 -0500

    altered box2 conditional statement

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 //When mouse over box 2, the color changes to orange
 
-//document.getElementById("box2").addEventListener("mouseover",changeColor);
+// document.getElementById("box2").addEventListener("mouseover",changeColor);
 
-//function changeColor() {
-    //document.getElementById("box2").style.backgroundColor="orange";
-//}
+// function changeColor() 
+// {
+//     document.getElementById("box2").style.backgroundColor="orange";
+// }
+// function changeColor1() 
+// {
+//     document.getElementById("box2").style.backgroundColor="blue";
+// }
 var box2 = document.getElementById("box2");
+var hover = document.getElementById("hover");
+var out = document.getElementById("out");
 
-box2.addEventListener("mouseover",function()
+hover.addEventListener("mouseover",function()
 {
-    if (box2.onmouseover == true) {
-        box2.style.backgroundColor="orange";
-    }
-    if (box2.onmouseover==false) {
-    box2.style.backgroundColor="blue";
+    if (hover.onmouseover == true)
+    {
+        box2.style.backgroundColor = "orange";
+    } 
+    if(out.onmouseover == false) 
+    {
+        box2.style.backgroundColor = "blue";
     }
 });
