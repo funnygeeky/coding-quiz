be1ff0ce9ea8a14ff353433792dcd54edcb80aa4
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-14 20:42:20 -0500

    Added heading,paragraph,and button to main html page

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //When mouse over box 2, the color changes to orange
 
-// document.getElementById("box2").addEventListener("mouseover",changeColor);
+document.getElementById("box2").addEventListener("mouseover",changeColor);
 
-// function changeColor() 
-// {
-//     document.getElementById("box2").style.backgroundColor="orange";
-// }
-// function changeColor1() 
-// {
-//     document.getElementById("box2").style.backgroundColor="blue";
-// }
-var box2 = document.getElementById("box2");
-var hover = document.getElementById("hover");
-var out = document.getElementById("out");
-
-hover.addEventListener("mouseover",function()
+function changeColor() 
 {
-    if (hover.onmouseover == true)
-    {
-        box2.style.backgroundColor = "orange";
-    } 
-    if(out.onmouseover == false) 
-    {
-        box2.style.backgroundColor = "blue";
-    }
-});
+    document.getElementById("box2").style.backgroundColor="orange";
+}
+function changeColor1() 
+{
+    document.getElementById("box2").style.backgroundColor="blue";
+}
+// var box2 = document.getElementById("box2");
+// var hover = document.getElementById("hover");
+// var out = document.getElementById("out");
+
+// hover.addEventListener("mouseover",function()
+// {
+//     if (hover.onmouseover == true)
+//     {
+//         box2.style.backgroundColor = "orange";
+//     } 
+//     if(out.onmouseover == false) 
+//     {
+//         box2.style.backgroundColor = "blue";
+//     }
+// });
