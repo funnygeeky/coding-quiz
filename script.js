1f27b7cbdc69d85ac5f7820c0148c5303b614456
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-14 21:53:55 -0500

    added divs for htmlcontent styling and Timer to display 75 seconds counting down.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //When mouse over box 2, the color changes to orange
 
-document.getElementById("box2").addEventListener("mouseover",changeColor);
+//document.getElementById("box2").addEventListener("mouseover",changeColor);
 
 function changeColor() 
 {
@@ -25,3 +25,30 @@ function changeColor1()
 //         box2.style.backgroundColor = "blue";
 //     }
 // });
+var timerEl = document.getElementById ('countdown');
+var timer = document.getElementById('timer');
+
+//Timer that counts down from 75
+function countdown () {
+    var timeLeft = 2;
+    //Use the 'setInterval()' method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval (function (){
+        //As long as the 'timeLeft' is great than 1
+        if (timeLeft > 1) {
+            //Set the 'textcontent of 'timerEL to show the remaining seconds
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            //Decrement timeLeft' by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            //When 'timeLeft is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timeLeft + ' second remaining';
+        }else {
+            //Once 'timeLeft gets to 0, set 'timerEl' to an empty string
+            timerEl.textContent = '';
+            //Use 'clearInterval()'to stop the timer
+            clearInterval(timeInterval);
+        }
+    },1000);
+}
+countdown();
+console.log ("countdown");
