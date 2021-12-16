64abfd740e8dbc6609a510ba6b85a5a3cc5d8b66
Michelle Wood <candybaswell@Candys-MacBook-Pro.local>
2021-12-15 19:03:46 -0500

    fixed timer behind start button countdown.

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-//When mouse over box 2, the color changes to orange
+//variables for timer and start quiz button
+var timerEl = document.getElementById('timer');
+var startQuizEl = document.querySelector(".btn");
+//
+//array to hold questions and answers text
+var questions = [
+    {
+        question: "What does API stand for?"
+        answers: [
+            {text: "Associated Programs International", correct: false },
+            {text: "Application Program Interval", correct: false },
+            // {text: "Application Programming Interface"}, correct: true},
+            {text: "Apple - Pi Integration", correct: false}
+        ]
+    }
 
-//document.getElementById("box2").addEventListener("mouseover",changeColor);
-
-function changeColor() 
-{
-    document.getElementById("box2").style.backgroundColor="orange";
-}
-function changeColor1() 
-{
-    document.getElementById("box2").style.backgroundColor="blue";
-}
-// var box2 = document.getElementById("box2");
-// var hover = document.getElementById("hover");
-// var out = document.getElementById("out");
-
-// hover.addEventListener("mouseover",function()
-// {
-//     if (hover.onmouseover == true)
-//     {
-//         box2.style.backgroundColor = "orange";
-//     } 
-//     if(out.onmouseover == false) 
-//     {
-//         box2.style.backgroundColor = "blue";
-//     }
-// });
-//var timerEl = document.getElementById ('countdown');
-// Attach an event listener to the 'start quiz' button element
-
-// Call the timer to countdown when the start quiz button is clicked
-var startQuizEl = document.querySelector("button");
-console.log (startQuizEl);
-startQuizEl.addEventListener("click", countdown ());
 
 
 //Timer that counts down from 75
 function countdown () {
-    var timerEl = document.getElementById('timer');
-    var timeLeft = 2;
+    console.log("countdown function being called")
+    
+    var timeLeft = 30;
     //Use the 'setInterval()' method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval (function (){
+        console.log(timeLeft);
+        timeLeft--;
         //As long as the 'timeLeft' is great than 1
         if (timeLeft > 1) {
             //Set the 'textcontent of 'timerEL to show the remaining seconds
@@ -53,6 +38,6 @@ function countdown () {
             timerEl.textContent = '';}
         },1000);
 }
-//Call the timer to countdown when the start quiz button is clicked
-// var startQuizEl = document.querySelector("button");
-// startQuizEl.addEventListener("click", countdown ());
+// Call the timer to countdown when the start quiz button is clicked
+
+startQuizEl.addEventListener("click", countdown);
